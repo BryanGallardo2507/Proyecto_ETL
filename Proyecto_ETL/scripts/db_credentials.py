5f844e31384a13ecff393044f9c81bfb1ae212dd
Bryan Gallardo <bryangallardo2550@gmail.com>
2025-03-29 21:37:31 -0600

    CORRECCION DE ERRORES EN CREDENTIALS

--- Proyecto_ETL/scripts/db_credentials.py
+++ Proyecto_ETL/scripts/db_credentials.py
@@ -1,13 +1,15 @@
+datawarehouse_name = 'DW_CHINOOK'
+
 #Conexion a la base de datos OLAP del Data Warehouse
 datawarehouse_db_config = {
     'driver': 'ODBC Driver 17 for SQL Server',
     'server': 'PC_RICARDO\SQLEXPRESS',
-    'database': 'DW_CHINOOK',
+    'database': datawarehouse_name,
     'user': 'ADMIN',
     'password': '123456',
     'autocommit': True,
 }
-
+#Conexion a la base de datos OLTP
 sqlserver_db_config = {
         'driver': 'ODBC Driver 17 for SQL Server',
         'server': 'PC_RICARDO\SQLEXPRESS',
